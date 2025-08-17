2acd90fae0ce6d3f0ed4b1dfcd2ebece61167cc4
Matthieu Napoli <matthieu@mnapoli.fr>
2025-08-17 18:54:28 +0200

    Improve landing

--- src/landing.ts
+++ src/landing.ts
@@ -59,9 +59,9 @@ export const landingPageHTML = `<!DOCTYPE html>
             
             <!-- Title -->
             <div class="mb-6 md:mb-8">
-                <div class="text-2xl sm:text-3xl sm:text-5xl md:text-6xl font-bold mb-2 flex items-center items-start space-x-2 sm:space-x-3 md:space-x-6">
+                <div class="text-2xl sm:text-3xl sm:text-5xl md:text-6xl font-bold mb-2 flex items-start space-x-2 sm:space-x-3 md:space-x-6">
                     <div>▓▓▓</div>
-                    <div class="pt-2">FAVICON.SHOW</div>
+                    <div class="pt-2.5">FAVICON.SHOW</div>
                     <div>▓▓▓</div>
                 </div>
                 <div class="text-sm sm:text-base sm:text-lg md:text-xl opacity-80">
